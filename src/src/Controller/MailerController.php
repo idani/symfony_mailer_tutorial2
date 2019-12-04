7e17f841fc116d0cacab976ec767028c0ce99d49
Shigehiro IDANI <idani@hirotae.com>
2019-12-04 14:24:55 +0900

    https://symfony.com/doc/4.4/mailer.html#creating-sending-messagesのサンプルに修正

--- src/src/Controller/MailerController.php
+++ src/src/Controller/MailerController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MailerController extends AbstractController
@@ -10,8 +12,21 @@ class MailerController extends AbstractController
     /**
      * @Route("/mailer", name="mailer")
      */
-    public function index()
+    public function index(MailerInterface $mailer)
     {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
         return $this->render('mailer/index.html.twig', [
             'controller_name' => 'MailerController',
         ]);
